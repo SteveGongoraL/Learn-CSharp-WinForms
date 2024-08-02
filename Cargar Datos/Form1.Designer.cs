69b78450212a6e0dd2e13d2268dd5ce38ff1030d
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-01 18:04:35 -0600

    Ejercicio cargando datos desde un JSON

---
+++ Cargar Datos/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace CargarDatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxDireccion = new System.Windows.Forms.TextBox();
+            this.comboBoxProducto = new System.Windows.Forms.ComboBox();
+            this.buttonGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dirección";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Producto";
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(141, 53);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(121, 20);
+            this.textBoxNombre.TabIndex = 3;
+            // 
+            // textBoxDireccion
+            // 
+            this.textBoxDireccion.Location = new System.Drawing.Point(141, 91);
+            this.textBoxDireccion.Name = "textBoxDireccion";
+            this.textBoxDireccion.Size = new System.Drawing.Size(121, 20);
+            this.textBoxDireccion.TabIndex = 4;
+            // 
+            // comboBoxProducto
+            // 
+            this.comboBoxProducto.FormattingEnabled = true;
+            this.comboBoxProducto.Location = new System.Drawing.Point(141, 126);
+            this.comboBoxProducto.Name = "comboBoxProducto";
+            this.comboBoxProducto.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxProducto.TabIndex = 5;
+            // 
+            // buttonGuardar
+            // 
+            this.buttonGuardar.Location = new System.Drawing.Point(125, 218);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(75, 23);
+            this.buttonGuardar.TabIndex = 6;
+            this.buttonGuardar.Text = "Guardar";
+            this.buttonGuardar.UseVisualStyleBackColor = true;
+            this.buttonGuardar.Click += new System.EventHandler(this.buttonGuardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 279);
+            this.Controls.Add(this.buttonGuardar);
+            this.Controls.Add(this.comboBoxProducto);
+            this.Controls.Add(this.textBoxDireccion);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cargar datos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxDireccion;
+        private System.Windows.Forms.ComboBox comboBoxProducto;
+        private System.Windows.Forms.Button buttonGuardar;
+    }
+}
+
---
+++ Cargar Datos/Form1.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CargarDatos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Shown += OnShown;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonGuardar_Click(object sender, EventArgs e)
+        {
+            int indice = comboBoxProducto.SelectedIndex;
+            string producto = Convert.ToString(comboBoxProducto.Items[indice]);
+
+            List<usuario> _data = new List<usuario>();
+            _data.Add(new usuario()
+            {
+                Nombre = textBoxNombre.Text,
+                Direccion = textBoxDireccion.Text,
+                Producto = producto
+            });
+
+            string json = JsonConvert.SerializeObject(_data.ToArray());
+
+            System.IO.File.AppendAllText(@".\Datos.json", json);
+
+            MessageBox.Show("Se guardaron los datos de compra");
+        }
+        public class usuario
+        {
+            public string Nombre { get; set; }
+            public string Direccion { get; set; }
+            public string Producto { get; set; }
+        }
+        public class ProductosC
+        {
+            public Datos[] Productos { get; set; }
+        }
+        public class Datos
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public int Cantidad { get; set; }
+            public override string ToString() => Nombre;
+        }
+        public class JsonOperations
+        {
+            public static ProductosC Read() =>
+            JsonConvert.DeserializeObject<ProductosC>(File.ReadAllText("ProductosC.json"));
+        }
+        private void OnShown(object sender, EventArgs e)
+        {
+            comboBoxProducto.DataSource = JsonOperations.Read().Productos.OrderBy(elemento => elemento.Nombre).ToList();
+        }
+    }
+}
