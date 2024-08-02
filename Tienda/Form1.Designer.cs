cc716eb56ba87ce92356776c4139ba112e8c0c13
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-01 23:46:15 -0600

    Ejercicio simulando un sistema de ventas

---
+++ Tienda/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace Productos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxProducto = new System.Windows.Forms.ComboBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dg1 = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPagar = new System.Windows.Forms.Button();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.cbxPrecios = new System.Windows.Forms.ComboBox();
+            this.btnVer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbxProducto
+            // 
+            this.cbxProducto.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxProducto.FormattingEnabled = true;
+            this.cbxProducto.Location = new System.Drawing.Point(111, 101);
+            this.cbxProducto.Name = "cbxProducto";
+            this.cbxProducto.Size = new System.Drawing.Size(127, 21);
+            this.cbxProducto.TabIndex = 88;
+            this.cbxProducto.SelectedIndexChanged += new System.EventHandler(this.cbxProducto_SelectedIndexChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.Purple;
+            this.btnAgregar.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAgregar.Location = new System.Drawing.Point(247, 202);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(148, 35);
+            this.btnAgregar.TabIndex = 87;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // dg1
+            // 
+            this.dg1.BackgroundColor = System.Drawing.Color.Purple;
+            this.dg1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dg1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Precio,
+            this.Cantidad,
+            this.Total});
+            this.dg1.Location = new System.Drawing.Point(8, 253);
+            this.dg1.Name = "dg1";
+            this.dg1.ReadOnly = true;
+            this.dg1.Size = new System.Drawing.Size(441, 182);
+            this.dg1.TabIndex = 86;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.Name = "Precio";
+            this.Precio.ReadOnly = true;
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            this.Cantidad.ReadOnly = true;
+            // 
+            // Total
+            // 
+            this.Total.HeaderText = "Total";
+            this.Total.Name = "Total";
+            this.Total.ReadOnly = true;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblTotal.ForeColor = System.Drawing.Color.Indigo;
+            this.lblTotal.Location = new System.Drawing.Point(243, 163);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(18, 20);
+            this.lblTotal.TabIndex = 85;
+            this.lblTotal.Text = "0";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label21.ForeColor = System.Drawing.Color.Indigo;
+            this.label21.Location = new System.Drawing.Point(182, 163);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(66, 20);
+            this.label21.TabIndex = 84;
+            this.label21.Text = "Precio $";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.ForeColor = System.Drawing.Color.Indigo;
+            this.label5.Location = new System.Drawing.Point(290, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 20);
+            this.label5.TabIndex = 82;
+            this.label5.Text = "Cantidad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.ForeColor = System.Drawing.Color.Indigo;
+            this.label3.Location = new System.Drawing.Point(32, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 20);
+            this.label3.TabIndex = 81;
+            this.label3.Text = "Producto";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(47, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(364, 30);
+            this.label1.TabIndex = 80;
+            this.label1.Text = "Bienvenido a la Tienda Funko";
+            // 
+            // btnPagar
+            // 
+            this.btnPagar.BackColor = System.Drawing.Color.Purple;
+            this.btnPagar.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPagar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnPagar.Location = new System.Drawing.Point(121, 457);
+            this.btnPagar.Name = "btnPagar";
+            this.btnPagar.Size = new System.Drawing.Size(210, 35);
+            this.btnPagar.TabIndex = 89;
+            this.btnPagar.Text = "Pagar";
+            this.btnPagar.UseVisualStyleBackColor = false;
+            this.btnPagar.Click += new System.EventHandler(this.btnPagar_Click);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(370, 102);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(53, 20);
+            this.txtCantidad.TabIndex = 90;
+            this.txtCantidad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCantidad_KeyPress);
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Location = new System.Drawing.Point(5, 490);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(10, 13);
+            this.lblProducto.TabIndex = 91;
+            this.lblProducto.Text = ".";
+            this.lblProducto.Visible = false;
+            // 
+            // cbxPrecios
+            // 
+            this.cbxPrecios.Enabled = false;
+            this.cbxPrecios.FormattingEnabled = true;
+            this.cbxPrecios.Location = new System.Drawing.Point(421, 490);
+            this.cbxPrecios.Name = "cbxPrecios";
+            this.cbxPrecios.Size = new System.Drawing.Size(24, 21);
+            this.cbxPrecios.TabIndex = 94;
+            this.cbxPrecios.Visible = false;
+            // 
+            // btnVer
+            // 
+            this.btnVer.BackColor = System.Drawing.Color.Purple;
+            this.btnVer.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVer.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnVer.Location = new System.Drawing.Point(93, 202);
+            this.btnVer.Name = "btnVer";
+            this.btnVer.Size = new System.Drawing.Size(148, 35);
+            this.btnVer.TabIndex = 95;
+            this.btnVer.Text = "Ver Precio";
+            this.btnVer.UseVisualStyleBackColor = false;
+            this.btnVer.Click += new System.EventHandler(this.btnVer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumPurple;
+            this.ClientSize = new System.Drawing.Size(457, 499);
+            this.Controls.Add(this.btnVer);
+            this.Controls.Add(this.cbxPrecios);
+            this.Controls.Add(this.lblProducto);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.btnPagar);
+            this.Controls.Add(this.cbxProducto);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dg1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Venta Funko Pop";
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxProducto;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dg1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPagar;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.ComboBox cbxPrecios;
+        private System.Windows.Forms.Button btnVer;
+    }
+}
+
---
+++ Tienda/Form1.cs
@@ -0,0 +1,189 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Productos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Shown += OnShown;
+            Shown += OnShown2;
+        }
+        int suma = 0;
+        //Cargar Productos
+        public class DatosLoops
+        {
+            public Datos[] Elementos { get; set; }
+        }
+        public class Datos
+        {
+            public string Nombre { get; set; }
+            public string Precio { get; set; }
+            public override string ToString() => Nombre;
+        }
+        public class JsonOperations
+        {
+            public static DatosLoops Read() =>
+            JsonConvert.DeserializeObject<DatosLoops>(File.ReadAllText("../../docs/Inventario.json"));
+        }
+        private void OnShown(object sender, EventArgs e)
+        {
+            cbxProducto.DataSource = JsonOperations.Read().Elementos.ToList();
+        }
+        //Cargar Precios
+        public class PrecioLoops
+        {
+            public DatosPrecio[] Elementos { get; set; }
+        }
+        public class DatosPrecio
+        {
+            public string Nombre { get; set; }
+            public string Precio { get; set; }
+            public override string ToString() => Precio;
+        }
+        public class JsonOperations2
+        {
+            public static PrecioLoops Read() =>
+            JsonConvert.DeserializeObject<PrecioLoops>(File.ReadAllText("../../docs/Inventario.json"));
+        }
+        private void OnShown2(object sender, EventArgs e)
+        {
+            cbxPrecios.DataSource = JsonOperations2.Read().Elementos.ToList();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            // Declarar variables
+            string producto, precioUnitario, cantidadProducto, totalProductoStr;
+            int totalProducto, indiceElemento;
+            
+            // Producto
+            indiceElemento = cbxProducto.SelectedIndex;
+            producto = Convert.ToString(cbxProducto.Items[indiceElemento]);
+            
+            // Precio unitario
+            precioUnitario = Convert.ToString(cbxPrecios.Items[indiceElemento]);
+            
+            // Cantidad a pedir
+            if(txtCantidad.Text == "")
+            {
+                cantidadProducto = "1";
+            }
+            else
+            {
+                cantidadProducto = Convert.ToString(txtCantidad.Text);
+            }
+
+            // Total
+            totalProducto = Convert.ToInt32(precioUnitario) * Convert.ToInt32(cantidadProducto);
+            totalProductoStr = Convert.ToString(totalProducto);
+
+            // Limpiar
+            txtCantidad.Text = string.Empty;
+            lblTotal.Text = string.Empty;
+
+            // Llenar la tabla
+            dg1.Rows.Add(producto, precioUnitario, cantidadProducto, totalProductoStr);
+
+            List<usuario> _data = new List<usuario>();
+            _data.Add(new usuario()
+            {
+                Nombre = producto,
+                Precio = precioUnitario,
+                Cantidad = cantidadProducto,
+                Total = totalProductoStr,
+            });
+            string json = JsonConvert.SerializeObject(_data.ToArray());
+            System.IO.File.AppendAllText(@"../../docs/RegistroCompras.json", json);
+        }
+        public class usuario
+        {
+            public string Nombre { get; set; }
+            public string Precio { get; set; }
+            public string Cantidad { get; set; }
+            public string Total { get; set; }
+
+        }
+        private void txtCantidad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnVer_Click(object sender, EventArgs e)
+        {
+            // Declarar variables
+            string producto, precioUnitario, cantidadProducto, totalProductoStr;
+            int totalProducto, indiceElemento;
+
+            // Producto
+            indiceElemento = cbxProducto.SelectedIndex;
+            producto = Convert.ToString(cbxProducto.Items[indiceElemento]);
+
+            // Precio unitario
+            precioUnitario = Convert.ToString(cbxPrecios.Items[indiceElemento]);
+
+            // Cantidad a pedir
+            if (txtCantidad.Text == "")
+            {
+                cantidadProducto = "1";
+            }
+            else
+            {
+                cantidadProducto = Convert.ToString(txtCantidad.Text);
+            }
+
+            // Total
+            totalProducto = Convert.ToInt32(precioUnitario) * Convert.ToInt32(cantidadProducto);
+            totalProductoStr = Convert.ToString(totalProducto);
+            lblTotal.Text = totalProductoStr;
+        }
+
+        private void btnPagar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dg1.RowCount - 1; i++)
+            {
+                suma += int.Parse(dg1.Rows[i].Cells[3].Value.ToString());
+            }
+            MessageBox.Show("El total de su compra es: $" + suma);
+            List<subtotal> _data = new List<subtotal>();
+            _data.Add(new subtotal()
+            {
+                Subtotal = Convert.ToString(suma),
+            });
+            string json = JsonConvert.SerializeObject(_data.ToArray());
+
+            //Escribiendo el archivo Json
+            System.IO.File.AppendAllText(@"../../docs/RegistroCompras.json", json);
+
+            // Limpiando la pagina
+            txtCantidad.Text = string.Empty;
+            lblTotal.Text = string.Empty;
+            dg1.Rows.Clear();
+            suma = 0;
+        }
+        public class subtotal
+        {
+            public string Subtotal { get; set; }
+        }
+
+        private void cbxProducto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtCantidad.Text = string.Empty;
+            lblTotal.Text = string.Empty;
+        }
+    }
+}
