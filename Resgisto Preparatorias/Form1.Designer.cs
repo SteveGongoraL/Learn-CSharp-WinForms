e9eb83dff000ff9a2db236ad82704676c1a3577a
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-01 18:36:55 -0600

    Ejercicio simulando un registro de nombre y preparatoria

---
+++ Resgisto Preparatorias/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace RegistroPreparatorias
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.cBoxCarga = new System.Windows.Forms.ComboBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblP = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Maiandra GD", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(74, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enviar y Cargar Datos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Handwriting", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(84, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Handwriting", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(84, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Edad";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(88, 97);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(199, 20);
+            this.txtNombre.TabIndex = 4;
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(88, 153);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(199, 20);
+            this.txtEdad.TabIndex = 6;
+            this.txtEdad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEdad_KeyPress);
+            // 
+            // cBoxCarga
+            // 
+            this.cBoxCarga.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBoxCarga.FormattingEnabled = true;
+            this.cBoxCarga.Location = new System.Drawing.Point(88, 210);
+            this.cBoxCarga.Name = "cBoxCarga";
+            this.cBoxCarga.Size = new System.Drawing.Size(199, 21);
+            this.cBoxCarga.TabIndex = 7;
+            this.cBoxCarga.SelectedIndexChanged += new System.EventHandler(this.cBoxCarga_SelectedIndexChanged);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(152, 300);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Handwriting", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(84, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Preparatoria";
+            // 
+            // lblP
+            // 
+            this.lblP.AutoSize = true;
+            this.lblP.Location = new System.Drawing.Point(-2, 338);
+            this.lblP.Name = "lblP";
+            this.lblP.Size = new System.Drawing.Size(10, 13);
+            this.lblP.TabIndex = 10;
+            this.lblP.Text = ".";
+            this.lblP.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(386, 351);
+            this.Controls.Add(this.lblP);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.cBoxCarga);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Evidencia 4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.ComboBox cBoxCarga;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblP;
+    }
+}
+
---
+++ Resgisto Preparatorias/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RegistroPreparatorias
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Shown += OnShown;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text == "" || txtEdad.Text == "")
+            {
+                MessageBox.Show("Es necesario llenar todos los campos");
+            }
+            else
+            {
+                List<usuario> _data = new List<usuario>();
+                _data.Add(new usuario()
+                {
+                    Nombre = txtNombre.Text,
+                    Edad = Convert.ToInt32(txtEdad.Text),
+                    Escuela = lblP.Text
+                });
+
+                string json = JsonConvert.SerializeObject(_data.ToArray());
+
+                System.IO.File.AppendAllText(@"..\..\docs\\DatosGuardados.json", json);
+
+                MessageBox.Show("Se han guardado los datos");
+            }
+        }
+        public class usuario
+        {
+            public string Nombre { get; set; }
+            public int Edad { get; set; }
+            public string Escuela { get; set; }
+        }
+        private void cBoxCarga_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int g = cBoxCarga.SelectedIndex;
+            string prepa = Convert.ToString(cBoxCarga.Items[g]);
+            lblP.Text = Convert.ToString(prepa);
+        }
+        private void txtEdad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && !char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        public class DatosLoops
+        {
+            public Datos[] Elementos { get; set; }
+        }
+        public class Datos
+        {
+            public string Nombre { get; set; }
+            public string Municipio { get; set; }
+            public override string ToString() => Nombre;
+        }
+        public class JsonOperations
+        {
+            public static DatosLoops Read() =>
+            JsonConvert.DeserializeObject<DatosLoops>(File.ReadAllText("..\\..\\docs\\DatosCarga.json"));
+        }
+        private void OnShown(object sender, EventArgs e)
+        {
+            cBoxCarga.DataSource = JsonOperations.Read().Elementos.OrderBy(elemento => elemento.Nombre).ToList();
+        }
+
+    }
+}
