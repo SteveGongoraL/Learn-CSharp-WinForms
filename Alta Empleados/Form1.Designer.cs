90491b0d8d14f787d22dcf255603ebdd40936bce
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-01 19:40:51 -0600

    Ejercicio dando de alta a empleados y generando reporte

---
+++ Alta Empleados/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace Empleados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            this.txtGenero = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbxArea = new System.Windows.Forms.ComboBox();
+            this.txtDomicilio = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 33);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Dar de Alta a Empleados";
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.BackColor = System.Drawing.Color.Purple;
+            this.btnRegistro.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistro.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnRegistro.Location = new System.Drawing.Point(98, 363);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(218, 35);
+            this.btnRegistro.TabIndex = 6;
+            this.btnRegistro.Text = "Registrar";
+            this.btnRegistro.UseVisualStyleBackColor = false;
+            this.btnRegistro.Click += new System.EventHandler(this.btnRegistro_Click);
+            // 
+            // txtGenero
+            // 
+            this.txtGenero.Location = new System.Drawing.Point(82, 205);
+            this.txtGenero.Name = "txtGenero";
+            this.txtGenero.Size = new System.Drawing.Size(250, 20);
+            this.txtGenero.TabIndex = 3;
+            this.txtGenero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(78, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 21);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Género";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(82, 109);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(250, 20);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(78, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 21);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Nombre";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(82, 157);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(250, 20);
+            this.txtEdad.TabIndex = 2;
+            this.txtEdad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEdad_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(78, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 21);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Edad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label5.Location = new System.Drawing.Point(78, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 21);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Área";
+            // 
+            // cbxArea
+            // 
+            this.cbxArea.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxArea.FormattingEnabled = true;
+            this.cbxArea.Location = new System.Drawing.Point(82, 301);
+            this.cbxArea.Name = "cbxArea";
+            this.cbxArea.Size = new System.Drawing.Size(250, 21);
+            this.cbxArea.TabIndex = 5;
+            // 
+            // txtDomicilio
+            // 
+            this.txtDomicilio.Location = new System.Drawing.Point(82, 253);
+            this.txtDomicilio.Name = "txtDomicilio";
+            this.txtDomicilio.Size = new System.Drawing.Size(250, 20);
+            this.txtDomicilio.TabIndex = 4;
+            this.txtDomicilio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDireccion_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label6.Location = new System.Drawing.Point(78, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 21);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Domicilió";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumPurple;
+            this.ClientSize = new System.Drawing.Size(411, 420);
+            this.Controls.Add(this.txtDomicilio);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbxArea);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnRegistro);
+            this.Controls.Add(this.txtGenero);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alta Empleados";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRegistro;
+        private System.Windows.Forms.TextBox txtGenero;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbxArea;
+        private System.Windows.Forms.TextBox txtDomicilio;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Alta Empleados/Form1.cs
@@ -0,0 +1,127 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Empleados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Shown += OnShown;
+        }
+
+        private void btnRegistro_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text == "" || txtEdad.Text == "" || txtGenero.Text == "" || txtDomicilio.Text == "")
+            {
+                MessageBox.Show("Es necesario llenar todos los campos para dar de alta");
+            }
+            else
+            {
+                int i = cbxArea.SelectedIndex;
+                string area = Convert.ToString(cbxArea.Items[i]);
+                
+                List<usuario> _data = new List<usuario>();
+                _data.Add(new usuario()
+                {
+                    Nombre = txtNombre.Text,
+                    Edad = Convert.ToInt32(txtEdad.Text),
+                    Genero = txtGenero.Text,
+                    Domicilio = txtDomicilio.Text,
+                    Area = area
+                });
+
+                string json = JsonConvert.SerializeObject(_data.ToArray());
+
+                if(area == "Finanzas")
+                {
+                    System.IO.File.AppendAllText(@"..\..\docs\RegistroFinanzas.json", json);
+                }
+                else if(area == "Recursos Humanos")
+                {
+                    System.IO.File.AppendAllText(@"..\..\docs\RegistroRH.json", json);
+                }
+                else if (area == "Tecnologia de Informacion")
+                {
+                    System.IO.File.AppendAllText(@"..\..\docs\RegistroTI.json", json);
+                }
+                else if (area == "Ventas")
+                {
+                    System.IO.File.AppendAllText(@"..\..\docs\RegistroVentas.json", json);
+                }
+                else
+                {
+                    System.IO.File.AppendAllText(@"..\..\docs\RegistroProduccion.json", json);
+                }
+
+                MessageBox.Show("Se ha registrado al empleado");
+                foreach (Control c in this.Controls)
+                {
+                    if (c is TextBox)
+                    {
+                        c.Text = "";
+                    }
+                }
+            }
+        }
+        public class usuario
+        {
+            public string Nombre { get; set; }
+            public int Edad { get; set; }
+            public string Genero { get; set; }
+            public string Domicilio { get; set; }
+            public string Area { get; set; }
+        }
+        //Cargar Areas
+        public class AreasLoops
+        {
+            public Areas[] Elementos { get; set; }
+        }
+        public class Areas
+        {
+            public int ID { get; set; }
+            public string Puesto { get; set; }
+            public override string ToString() => Puesto;
+        }
+        public class JsonOperations
+        {
+            public static AreasLoops Read() =>
+            JsonConvert.DeserializeObject<AreasLoops>(File.ReadAllText("..\\..\\docs\\AreasCarga.json"));
+        }
+        private void OnShown(object sender, EventArgs e)
+        {
+            cbxArea.DataSource = JsonOperations.Read().Elementos.ToList();
+        }
+        private void txtEdad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && !char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        private void txtDireccion_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetterOrDigit(e.KeyChar) && !char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
